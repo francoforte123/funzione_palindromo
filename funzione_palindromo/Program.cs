0c424ce8cc529cfc262d1d688534d42bed180b84
Emmanuele La Duca <laducaemanuele20@gmail.com>
2023-06-20 17:05:59 +0200

    Aggiungere i file di progetto.

---
+++ funzione_palindromo/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("insert the text= ");
+String text = Console.ReadLine();
+
+Console.WriteLine("\n");
+Console.WriteLine("you have insert this text= " + text);
+
+String resultText = " ";
+String readyText = "anna";
+
+for (int i = text.Length; i > 0; i--)
+{
+    resultText += text[i - 1];
+}
+
+Console.WriteLine("the result text is= " + resultText);
+
+if (text == readyText)
+{
+    Console.WriteLine("\nthe insert text is polindrome");
+}
+else {
+    Console.WriteLine("\nthe text is not polindrome");
+}
